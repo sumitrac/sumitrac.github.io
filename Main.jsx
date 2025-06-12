e2faa8ed3d5334294bc1e50de1f0240335597936
Sumitra <sumitrac2008@gmail.com>
2025-06-12 14:36:07 -0700

    changes to files

---
+++ Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <header>
+                <h1>Projects</h1>
+                <h1>Contact</h1>
+            </header>
+        </div>
+    );
+};
+
+export default Main;
